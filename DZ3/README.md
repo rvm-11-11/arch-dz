c50d90179f48d1290b33ef3aaa1ca1756d2a6cf2
rvm-11-11 <K4hegR4b75FbTN4>
2020-12-14 23:58:11 +0300

    wrapping app in Docker and Helm

--- DZ3/README.md
+++ DZ3/README.md
@@ -20,6 +20,20 @@ kubectl port-forward service/prom-grafana 9000:80
 http://localhost:9000/login -- admin:prom-operator
 kubectl port-forward service/prom-kube-prometheus-stack-prometheus 9090
 http://localhost:9090/
+
+./mvnw package
+java -jar target/dz3-spring-app-1.0.0.jar
+docker build --tag rvm1111/arch:dz3v1 .
+docker run --publish 8080:8080 --detach rvm1111/arch:dz3v1
+docker push rvm1111/arch:dz3v1
+docker stop rvm1111/arch:dz3v1
+docker rm rvm1111/arch:dz3v1
+docker login
+
+helm install my-helm-app ./my-helm-app
+helm upgrade my-helm-app ./my-helm-app --atomic
+kubectl get servicemonitors.monitoring.coreos.com
+kubectl describe servicemonitors.monitoring.coreos.com my-helm-app-myapp  
 ```
 
 
--- DZ3/spring-app/src/main/java/org/openapitools/api/HealthcheckController.java
+++ DZ3/spring-app/src/main/java/org/openapitools/api/HealthcheckController.java
@@ -1,16 +1,33 @@
 package org.openapitools.api;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
 @Slf4j
 public class HealthcheckController {
 
+    @Value("${my.version}")
+    private String version;
+
     @RequestMapping("/health")
-    public String health() {
+    public Map<String, String> health() {
         log.info("Calling health()");
-        return "{\"status\": \"OK\", \"version\": \"v3\"}";
+        Map<String, String> response = new HashMap();
+        response.put("status", "OK");
+        return response;
+    }
+
+    @RequestMapping("/version")
+    public Map<String, String> version() {
+        log.info("Calling version()");
+        Map<String, String> response = new HashMap();
+        response.put("version", version);
+        return  response;
     }
 }

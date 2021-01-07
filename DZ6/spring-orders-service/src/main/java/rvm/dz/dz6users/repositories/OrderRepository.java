edd6548c30e4b222d536ec444be5225e5292b057
rvm-11-11 <K4hegR4b75FbTN4>
2021-01-07 22:48:53 +0300

    basic implementation for Orders service

---
+++ DZ6/spring-orders-service/src/main/java/rvm/dz/dz6users/repositories/OrderRepository.java
@@ -0,0 +1,7 @@
+package rvm.dz.dz6users.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface OrderRepository extends JpaRepository<OrderEntity, Long> {
+
+}
